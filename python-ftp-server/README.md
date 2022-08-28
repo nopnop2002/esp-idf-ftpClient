e216e0be4ea7c2792e58dc0881e13358e976a0b4
nopnop2002 <hau26280@tree.odn.ne.jp>
2022-08-28 09:44:11 +0900

    Added python-ftp-server

---
+++ python-ftp-server/README.md
@@ -0,0 +1,14 @@
+# FTP Server using python
+
+You can build FTP Server using python.   
+
+```
+git clone https://github.com/nopnop2002/esp-idf-ftpClient
+cd esp-idf-ftpClient/python-ftp-server
+python3 -m pip install pyftpdlib
+python3 main.py
+```
+
+
+![ftp-server-1](https://user-images.githubusercontent.com/6020549/187052904-93b8c40c-e1ea-4082-b381-30843fe48c90.jpg)
+
---
+++ python-ftp-server/main.py
@@ -0,0 +1,42 @@
+# python3 -m pip install pyftpdlib
+import pyftpdlib.authorizers
+import pyftpdlib.handlers
+import pyftpdlib.servers
+import argparse
+import logging
+import os
+
+from pyftpdlib.handlers import FTPHandler
+
+class Handler(FTPHandler):
+    def on_file_sent(self, file):
+        logging.info("send file! {}".format(file))
+        #super(Handler, self).ftp_RETR(file)
+    def on_file_received(self, file):
+        logging.info("received file! {}".format(file))
+        #super(Handler, self).ftp_RETR(file)
+
+
+logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.INFO)
+parser = argparse.ArgumentParser()
+parser.add_argument('--user', default="user", help='ftp user name')
+parser.add_argument('--password', default="password", help='ftp user password')
+parser.add_argument('--port', default=2121, help='ftp port')
+args = parser.parse_args()
+logging.info("user={} {}".format(type(args.user), args.user))
+logging.info("password={} {}".format(type(args.password), args.password))
+logging.info("port={} {}".format(type(args.port), args.port))
+
+
+authorizer = pyftpdlib.authorizers.DummyAuthorizer()
+#authorizer.add_user('user', 'password', '/home/nop/ftp-server', perm='elradfmw')
+authorizer.add_user(args.user, args.password, os.getcwd(), perm='elradfmw')
+handler = Handler
+logging.info(handler.passive_ports)
+handler.authorizer = authorizer
+# Disable remote connection
+#server = pyftpdlib.servers.FTPServer(("127.0.0.1", 2121), handler)
+# Enable remote connection
+server = pyftpdlib.servers.FTPServer(("0.0.0.0", 2121), handler)
+server.serve_forever()
+
